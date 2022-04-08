09e581b3f1de16ba3164e5ff79225b573079c3a8
Rasmus Munk <munk1@live.dk>
2022-04-08 17:09:23 +0200

    Update the deploy-image.sh to the new Makefile structure

--- deploy-image.sh
+++ deploy-image.sh
@@ -7,12 +7,12 @@ if [[ -n ${DOCKERHUB_USERNAME} ]] && [[ -n ${DOCKERHUB_PASSWORD} ]]; then
     echo "${DOCKERHUB_PASSWORD}" | docker login -u ${DOCKERHUB_USERNAME} --password-stdin
 fi
 
-make build TAG=${TAG} ARGS=${BUILD_ARGS}
-make push TAG=${TAG} ARGS=${BUILD_ARGS}
+make TAG=${TAG} ARGS=${BUILD_ARGS}
+make dockerpush TAG=${TAG} ARGS=${BUILD_ARGS}
 
 # If the EXTRA_TAG is set.
 # Link it to the original tag and push that version as well
 if [[ -n ${EXTRA_TAG} ]]; then
-    make build TAG=${!EXTRA_TAG:0:8} ARGS=${BUILD_ARGS}
-    make push TAG=${!EXTRA_TAG:0:8} ARGS=${BUILD_ARGS}
+    make TAG=${!EXTRA_TAG:0:8} ARGS=${BUILD_ARGS}
+    make dockerpush TAG=${!EXTRA_TAG:0:8} ARGS=${BUILD_ARGS}
 fi

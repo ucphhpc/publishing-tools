d5d7658621938c5ad008e189e1a29f59fa3f5632
Rasmus <rasmunk@users.noreply.github.com>
2022-04-12 21:14:13 +0200

    Update deploy-image.sh

--- deploy-image.sh
+++ deploy-image.sh
@@ -7,6 +7,9 @@ if [[ -n ${DOCKERHUB_USERNAME} ]] && [[ -n ${DOCKERHUB_PASSWORD} ]]; then
     echo "${DOCKERHUB_PASSWORD}" | docker login -u ${DOCKERHUB_USERNAME} --password-stdin
 fi
 
+# Prepare for a clean build
+make clean
+# Build from scratch and push
 make TAG=${TAG} ARGS=${BUILD_ARGS}
 make dockerpush TAG=${TAG} ARGS=${BUILD_ARGS}
 

0e4166c57c0595bc47b89ce2d1e6593a15c53169
Rasmus <rasmunk@users.noreply.github.com>
2022-04-29 16:05:10 +0200

    Update deploy-notebook.sh

--- deploy-notebook.sh
+++ deploy-notebook.sh
@@ -8,6 +8,9 @@ if [[ -n ${DOCKERHUB_USERNAME} ]] && [[ -n ${DOCKERHUB_PASSWORD} ]]; then
     echo "${DOCKERHUB_PASSWORD}" | docker login -u ${DOCKERHUB_USERNAME} --password-stdin
 fi
 
+# Prepare for a clean build
+make clean
+# Build from scratch and push
 make build/${NOTEBOOK} TAG=${TAG} ARGS=${BUILD_ARGS}
 make push/${NOTEBOOK} TAG=${TAG} ARGS=${BUILD_ARGS}
 

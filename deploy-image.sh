d0b70a40429504c479b3819ae634a68fa8cab8ae
Rasmus Munk <munk1@live.dk>
2023-01-27 14:38:33 +0100

    Reorder the COMMIT_TAG and EXTRA_TAG order since the COMMIT_TAG is the required default and the EXTRA_TAG is the optional addition

--- deploy-image.sh
+++ deploy-image.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 BUILD_ARGS=$1
 TAG=$2
-EXTRA_TAG=$3
-COMMIT_TAG=$4
-
+COMMIT_TAG=$3
+EXTRA_TAG=$4
 
 if [[ -n ${DOCKERHUB_USERNAME} ]] && [[ -n ${DOCKERHUB_PASSWORD} ]]; then
     echo "${DOCKERHUB_PASSWORD}" | docker login -u ${DOCKERHUB_USERNAME} --password-stdin
@@ -15,16 +14,16 @@ make clean
 make TAG=${TAG} ARGS=${BUILD_ARGS}
 make dockerpush TAG=${TAG} ARGS=${BUILD_ARGS}
 
-# If the EXTRA_TAG is set.
-# Link it to the original tag and push that version as well
-if [[ -n ${EXTRA_TAG} ]]; then
-    make TAG=${EXTRA_TAG} ARGS=${BUILD_ARGS}
-    make dockerpush TAG=${EXTRA_TAG} ARGS=${BUILD_ARGS}
-fi
-
 # If the COMMIT_TAG is set.
 # Link it to the original tag and push that version as well
 if [[ -n ${COMMIT_TAG} ]]; then
     make TAG=${!COMMIT_TAG:0:8} ARGS=${BUILD_ARGS}
     make dockerpush TAG=${!COMMIT_TAG:0:8} ARGS=${BUILD_ARGS}
+fi
+
+# If the EXTRA_TAG is set.
+# Link it to the original tag and push that version as well
+if [[ -n ${EXTRA_TAG} ]]; then
+    make TAG=${EXTRA_TAG} ARGS=${BUILD_ARGS}
+    make dockerpush TAG=${EXTRA_TAG} ARGS=${BUILD_ARGS}
 fi
--- deploy-notebook.sh
+++ deploy-notebook.sh
@@ -2,8 +2,8 @@
 NOTEBOOK=$1
 BUILD_ARGS=$2
 TAG=$3
-EXTRA_TAG=$4
-COMMIT_TAG=$5
+COMMIT_TAG=$4
+EXTRA_TAG=$5
 
 if [[ -n ${DOCKERHUB_USERNAME} ]] && [[ -n ${DOCKERHUB_PASSWORD} ]]; then
     echo "${DOCKERHUB_PASSWORD}" | docker login -u ${DOCKERHUB_USERNAME} --password-stdin
@@ -15,15 +15,6 @@ make clean
 make build/${NOTEBOOK} TAG=${TAG} ARGS=${BUILD_ARGS}
 make push/${NOTEBOOK} TAG=${TAG} ARGS=${BUILD_ARGS}
 
-# If the EXTRA_TAG is set.
-# Link it to the original tag and push that version as well
-if [[ -n ${EXTRA_TAG} ]]; then
-    ln -s Dockerfile.${TAG} ${NOTEBOOK}/Dockerfile.${EXTRA_TAG}
-
-    make build/${NOTEBOOK} TAG=${EXTRA_TAG} ARGS=${BUILD_ARGS}
-    make push/${NOTEBOOK} TAG=${EXTRA_TAG} ARGS=${BUILD_ARGS}
-fi
-
 # If the COMMIT_TAG is set.
 # Link it to the original tag and push that version as well
 if [[ -n ${COMMIT_TAG} ]]; then
@@ -31,4 +22,13 @@ if [[ -n ${COMMIT_TAG} ]]; then
 
     make TAG=${!COMMIT_TAG:0:8} ARGS=${BUILD_ARGS}
     make dockerpush TAG=${!COMMIT_TAG:0:8} ARGS=${BUILD_ARGS}
+fi
+
+# If the EXTRA_TAG is set.
+# Link it to the original tag and push that version as well
+if [[ -n ${EXTRA_TAG} ]]; then
+    ln -s Dockerfile.${TAG} ${NOTEBOOK}/Dockerfile.${EXTRA_TAG}
+
+    make build/${NOTEBOOK} TAG=${EXTRA_TAG} ARGS=${BUILD_ARGS}
+    make push/${NOTEBOOK} TAG=${EXTRA_TAG} ARGS=${BUILD_ARGS}
 fi

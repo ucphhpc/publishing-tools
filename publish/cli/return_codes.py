f920650a908d72f7b16579d65346ffea7097e80c
Rasmus Munk <rasmunk@users.noreply.github.com>
2024-11-08 14:13:10 +0100

    Update the copyright statement to be assigned to the Science HPC Center at UCPH

--- publish/cli/return_codes.py
+++ publish/cli/return_codes.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2024  rasmunk
+# Copyright (C) 2024  The publishing-tools Project by the Science HPC Center at UCPH
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
